aa4c53352a3e5471d6afaeef4c1010921a14f3bb
tracer-ics2017 <tracer@njuics.org>
2018-03-27 16:28:23 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:25:58 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:28:23 up  4:14,  1 user,  load average: 0.49, 0.28, 0.21 e5dd5d01a4258840d9578372caa09d305a88930c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -242,5 +242,5 @@ uint32_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   printf("nr_token:%d\n",nr_token);
-	return eval(1,nr_token);
+	return eval(0,nr_token);
 }
