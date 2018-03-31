4fbcdb710b988f95e739065a2a344899057c11bf
tracer-ics2017 <tracer@njuics.org>
2018-03-31 12:37:54 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:25:58 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:37:54 up  4:08,  1 user,  load average: 0.14, 0.25, 0.18 d368ec5e464a189942ca0401ed7fe7056a63b025

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
 #include "monitor/monitor.h"
+#include "monitor/watchpoint.h"
 
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
@@ -29,6 +30,11 @@ void cpu_exec(uint64_t n) {
 
 #ifdef DEBUG
     /* TODO: check watchpoints here. */
+    bool flag=judgeWatchPoint();
+    if(flag)
+      nemu_state=NEMU_STOP;
+    if(nemu_state!=NEMU_RUNNING)
+      return;
 
 #endif
 
