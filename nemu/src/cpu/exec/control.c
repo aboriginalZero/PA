9e47f4e6e21e73e7eb15600780e734cc7e4c120c
tracer-ics2017 <tracer@njuics.org>
2018-04-19 23:34:08 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:03:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:34:08 up  6:24,  1 user,  load average: 0.62, 0.37, 0.24 38bd9ec9d0d143276bbea9c8fa666426cb8086

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -27,7 +27,7 @@ make_EHelper(call) {
   // the target address is calculated at the decode stage
   
   rtl_push(&decoding.seq_eip);
-  rtl_subi(&decoding.jmp_eip,eip,id_dest->val);
+  rtl_addi(&decoding.jmp_eip,eip,id_dest->val);
   decoding.is_jmp = 1;
   print_asm("call %x", decoding.jmp_eip);
 }
