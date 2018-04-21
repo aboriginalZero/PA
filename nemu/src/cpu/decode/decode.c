74100a8642dfa236a242ca4567bf6748e6a7863c
tracer-ics2017 <tracer@njuics.org>
2018-04-21 08:50:06 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:03:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:50:06 up 47 min,  1 user,  load average: 0.30, 0.23, 0.29 896d8696afe949483c8e5e2239bf1da3d48cb553

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -39,7 +39,7 @@ static inline make_DopHelper(SI) {
    op->simm = ???
    */
   
-  op->simm=instr_fetch(&decoding.jmp_eip,op->width);
+  op->simm=instr_fetch(eip,op->width);
 
   rtl_li(&op->val, op->simm);
 
