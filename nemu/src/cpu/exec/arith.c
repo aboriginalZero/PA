63384223e30dfacde4e02b2d43d2ffc1827403c4
tracer-ics2017 <tracer@njuics.org>
2018-04-29 16:54:41 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:54:41 up  3:20,  1 user,  load average: 0.42, 0.29, 0.25 8e62be7287305b13ab704f66bf7fe33326946553

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -2,17 +2,18 @@
 
 make_EHelper(add) {
   rtl_add(&t2, &id_dest->val, &id_src->val);//t2存放相加结果
-  rtl_sltu(&t3, &id_dest->val, &t2);//t3为1则id_src->val为正数
+  
   //rtl_get_CF(&t1);
   
   operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
 
-  rtl_sltu(&t0, &id_dest->val, &t2);
-  rtl_or(&t0, &t3, &t0);
+  rtl_sltu(&t0,&t2,&id_dest->val);
+  
   rtl_set_CF(&t0);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_not(&t0);
   rtl_xor(&t1, &id_dest->val, &t2);
   rtl_and(&t0, &t0, &t1);
   rtl_msb(&t0, &t0, id_dest->width);
