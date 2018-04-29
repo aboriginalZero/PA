fdc30f0d082b185dc1300cd776939dc83617f0fe
tracer-ics2017 <tracer@njuics.org>
2018-04-29 14:53:47 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:53:47 up  1:20,  1 user,  load average: 0.20, 0.31, 0.52 95395af9d9321e80962a27125f6983904e0f34c6

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -17,11 +17,13 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_O:
     case CC_B:
     case CC_E:
+      rtl_get_ZF(dest);
+      break;
     case CC_BE:
     case CC_S:
     case CC_L:
     case CC_LE:
-    rtl_get_ZF(dest);
+    TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
