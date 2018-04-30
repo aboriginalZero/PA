44f880fab41b467300a616c415eec40db1992d5b
tracer-ics2017 <tracer@njuics.org>
2018-04-30 15:44:37 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:44:37 up 1 day,  2:10,  1 user,  load average: 0.12, 0.21, 0.18 80a450d2c46eb1e3d85aaacb106a4c8f5bab6178

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -42,6 +42,7 @@ make_EHelper(sub) {
 
 make_EHelper(cmp) {
   rtl_sub(&t2, &id_dest->val, &id_dest->val);
+  printf("id_dest->val:%d",id_dest->val);
   rtl_update_ZFSF(&t2, id_dest->width);
   rtl_sltu(&t0, &id_dest->val, &t2);
   // rtl_get_CF(&t1);
