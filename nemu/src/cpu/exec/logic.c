5ab5221b964b870b5f751228c19fde0a6ab5dced
tracer-ics2017 <tracer@njuics.org>
2018-04-30 16:09:19 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:09:19 up 1 day,  2:35,  1 user,  load average: 0.50, 0.47, 0.37 d5ddd1d8fca4fc46dbe1423914ee70701943e0ba

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -30,8 +30,8 @@ make_EHelper(xor) {
 
 make_EHelper(or) {
   rtl_or(&t2, &id_dest->val, &id_src->val);
-    printf("id_dest->val:%d\n",id_dest->val);
-  printf("id_src->val:%d\n",id_src->val);
+    printf("id_dest->val:%x\n",id_dest->val);
+  printf("id_src->val:%x\n",id_src->val);
   operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2,id_dest->width);
   rtl_set_OF(&tzero);
