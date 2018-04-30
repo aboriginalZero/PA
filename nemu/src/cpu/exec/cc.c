886311bf770beee25f149a38eaa8b295e860d4d9
tracer-ics2017 <tracer@njuics.org>
2018-04-30 20:54:44 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:54:44 up 1 day,  7:21,  1 user,  load average: 0.32, 0.32, 0.41 43ef11eed3a3202f52d91bc9cc24b49ba918620

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -3,7 +3,7 @@
 /* Condition Code */
 
 void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
-  rtlreg_t temp,temp1,temp2,temp3,temp4;
+  rtlreg_t temp,temp1,temp2,temp3;
   bool invert = subcode & 0x1;
   
   enum {
@@ -16,37 +16,41 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-        rtl_get_OF(dest);
-        break;
-    case CC_B:
-        rtl_get_CF(dest);
-        break;
+    case CC_O:{
+      rtl_get_OF(dest);
+      break;
+    }  
+    case CC_B:{
+      rtl_get_CF(dest);
+      break;
+    }   
     case CC_E:{
       rtl_get_ZF(dest);
       // printf("cpu.ZF:%d\n",*dest);
       break;
     }  
-    case CC_BE:
-        rtl_get_CF(&temp);
-        rtl_get_ZF(&temp1);
-        rtl_or(dest,&temp,&temp1);
-        break;
-    case CC_S:
-        rtl_get_SF(dest);
-        break;
-    case CC_L:
-        rtl_get_SF(&temp);
-        rtl_get_OF(&temp1);
-        *dest=(temp!=temp1);
-        break;
+    case CC_BE:{
+      rtl_get_CF(&temp);
+      rtl_get_ZF(&temp1);
+      rtl_or(dest,&temp,&temp1);
+      break;
+    }  
+    case CC_S:{
+      rtl_get_SF(dest);
+      break;
+    }  
+    case CC_L:{
+      rtl_get_SF(&temp);
+      rtl_get_OF(&temp1);
+      rtl_li(dest,(temp!=temp1));
+      break;
+    }      
     case CC_LE:
         rtl_get_ZF(&temp);
-        rtl_eqi(&temp1,&temp,1);
-        rtl_get_SF(&temp2);
-        rtl_get_OF(&temp3);
-        temp4=temp2!=temp3?0:1;
-        rtl_or(dest,&temp1,&temp4);
+        rtl_get_SF(&temp1);
+        rtl_get_OF(&temp2);
+        temp3=(temp1!=temp2?1:0);//这边有问题
+        rtl_or(dest,&temp,&temp3);
         break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
