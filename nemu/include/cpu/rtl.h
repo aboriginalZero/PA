1a80240b1d5b69035312214f6c6402406cf6f308
tracer-ics2017 <tracer@njuics.org>
2018-04-30 15:48:24 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:48:24 up 1 day,  2:14,  1 user,  load average: 0.20, 0.25, 0.20 f8207471378a9abc78ac28f08649b2b73a75e1d6

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -200,8 +200,10 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  rtl_msb(&t1,result,width);
-  rtl_set_SF(&t1);
+  rtlreg_t temp=0;
+  temp=(*result>>(width*8-1))&0x1;
+  // rtl_msb(&t1,result,width);
+  rtl_set_SF(&temp);
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
