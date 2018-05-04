8fce1b605229aad3fe3c16fb7527562e5771eb17
tracer-ics2017 <tracer@njuics.org>
2018-05-04 21:00:11 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:00:11 up 10:26,  1 user,  load average: 0.28, 0.26, 0.72 6c69eaf0c504095a734beed307cf7c3444b31da

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -81,7 +81,7 @@ opcode_entry opcode_table [512] = {
   /* 0x18 */	IDEXW(G2E,sbb,1), IDEXW(G2E,sbb,0), IDEXW(E2G,sbb,1), IDEXW(E2G,sbb,0),
   /* 0x1c */	IDEXW(I2a,sbb,1), IDEXW(I2a,sbb,0), EMPTY, EMPTY,
   /* 0x20 */	EMPTY, EMPTY, IDEXW(E2G,and,1), EMPTY,
-  /* 0x24 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x24 */	EMPTY, IDEX(I2a, and), EMPTY, EMPTY,
   /* 0x28 */	IDEXW(G2E,sub,1), IDEXW(G2E,sub,0), IDEXW(E2G,sub,1), IDEXW(E2G,sub,0),
   /* 0x2c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x30 */	IDEXW(G2E,xor,1), IDEX(G2E,xor), IDEXW(E2G,xor,1), IDEX(E2G,xor),
