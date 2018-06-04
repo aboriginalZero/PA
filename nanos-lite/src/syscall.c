27d1e5d578629bb149849b6ee25dadda084fbd0d
tracer-ics2017 <tracer@njuics.org>
2018-06-04 17:24:41 +0800

    >  run 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:24:41 up  5:16,  1 user,  load average: 0.16, 0.23, 0.25 823161c089ab73a253b1e155eca62b5efbd4a3a2

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,29 +1,31 @@
 #include "common.h"
 #include "syscall.h"
 
-_RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4],result=-1;
-  a[0] = SYSCALL_ARG1(r);
-  a[1] = SYSCALL_ARG2(r);
-	a[2] = SYSCALL_ARG3(r);
-	a[3] = SYSCALL_ARG4(r);
 
-
+extern char _end;
 uintptr_t sys_write(int fd, const void *buf, size_t len) {
 	uintptr_t i = 0;
 	if (fd == 1 || fd == 2) {
 		for(; len > 0; len--) {
 			_putc(((char*)buf)[i]);
       Log("it's log\n");
-			i++;;
+			i++;
 		}
 	}
 	return i;
 }
 
+_RegSet* do_syscall(_RegSet *r) {
+  uintptr_t a[4],ret=-1;
+  a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+	a[2] = SYSCALL_ARG3(r);
+	a[3] = SYSCALL_ARG4(r);
+
+
   switch (a[0]) {
     case SYS_none:{
-      result = 1; 
+      ret = 1; 
       break;
     }
     case SYS_exit:{
@@ -31,12 +33,12 @@ uintptr_t sys_write(int fd, const void *buf, size_t len) {
       break;
     }
     case SYS_write:{
-      result = sys_write(a[1], (void *)a[2], a[3]);
-			// result = fs_write(a[1], (void *)a[2], a[3]);
+      ret = sys_write(a[1], (void *)a[2], a[3]);
+			// ret = fs_write(a[1], (void *)a[2], a[3]);
 			break;  
     }
     case SYS_brk:{
-      result = 0;
+      ret = 0;
 			break;
     }
 			
@@ -44,7 +46,7 @@ uintptr_t sys_write(int fd, const void *buf, size_t len) {
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
-  SYSCALL_ARG1(r) = result;
+  SYSCALL_ARG1(r) = ret;
   
   return NULL;
 }
