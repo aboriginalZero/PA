0d34c5de399fe2784f9f52fedc9d0bc67da49816
tracer-ics2017 <tracer@njuics.org>
2018-06-04 16:55:15 +0800

    >  compile 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:55:15 up  4:46,  1 user,  load average: 0.58, 0.35, 0.27 a486b05a448695206db6ecd9d9fd055ca87fdfcb

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,21 +2,41 @@
 #include "syscall.h"
 
 _RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4];
+  uintptr_t a[4],result=-1;
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+	a[2] = SYSCALL_ARG3(r);
+	a[3] = SYSCALL_ARG4(r);
+
+
+uintptr_t sys_write(int fd, const void *buf, size_t len) {
+	uintptr_t i = 0;
+	if (fd == 1 || fd == 2) {
+		for(; len > 0; len--) {
+			_putc(((char*)buf)[i]);
+			i++;;
+		}
+	}
+	return i;
+}
 
   switch (a[0]) {
     case SYS_none:{
-      SYSCALL_ARG1(r) = 1; 
+      result = 1; 
       break;
     }
     case SYS_exit:{
-      _halt(SYSCALL_ARG2(r));
+      _halt(a[1]);
       break;
     }
-      
+    case SYS_write:{
+      result = sys_write(a[1], (void *)a[2], a[3]);
+			// result = fs_write(a[1], (void *)a[2], a[3]);
+			break;  
+    }
+			
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
-
+  SYSCALL_ARG1(r) = result;
   return NULL;
 }
