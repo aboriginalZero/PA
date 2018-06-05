87ad4356400042554bf358912fc530349cfbce9a
tracer-ics2017 <tracer@njuics.org>
2018-06-05 21:20:55 +0800

    >  run 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:20:55 up  9:06,  1 user,  load average: 0.49, 0.40, 0.39 96123e7f4eca8bcc941627dc135dbfd396915bea

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,13 +64,13 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
 		  file_table[fd].open_offset += len;
 		  break;
     default:
-      panic("fs_read函数出错\n");
-		  // if(file_table[fd].open_offset >= file_table[fd].size)
-	    //       return 0;
-		  // if(file_table[fd].open_offset + len > file_table[fd].size)
-		  //   len = file_table[fd].size - file_table[fd].open_offset;
-	    //   ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-		  //   file_table[fd].open_offset += len;
+      // panic("fs_read函数出错\n");
+		  if(file_table[fd].open_offset >= file_table[fd].size)
+	          return 0;
+		  if(file_table[fd].open_offset + len > file_table[fd].size)
+		    len = file_table[fd].size - file_table[fd].open_offset;
+	      ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+		    file_table[fd].open_offset += len;
   }
   return len;
 }
