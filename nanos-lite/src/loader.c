77297673a2cd70752e6a6bfa3637eb6d3a5a5785
tracer-ics2017 <tracer@njuics.org>
2018-06-05 21:30:04 +0800

    >  run 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:30:04 up  9:15,  1 user,  load average: 0.42, 0.53, 0.45 b8896220771ee87ffc584d86766edc33c95f51b7

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,31 +1,23 @@
 #include "common.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
+
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
-// extern void ramdisk_read(void *buf, off_t offset, size_t len);
-extern int fs_open(const char *pathname, int flags, int mode);
 extern ssize_t fs_read(int fd, void *buf, size_t len);
 extern size_t fs_filesz(int fd);
-extern void * new_page();
+extern int fs_open(const char *pathname, int flags, int mode);
+extern int fs_close(int fd);
+
 uintptr_t loader(_Protect *as, const char *filename) {
   // //将ramdisk中从0开始的所有内容放置在0x4000000,并
   // //把这个地址作为程序的入口返回即可.
   // ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
 
-  filename = "/bin/hello";
-  // int fd = fs_open(filename, 0, 0);
-  // fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
   int fd = fs_open(filename, 0, 0);
-  int filesize = fs_filesz(fd);
-  void *pa;
-  void *va = DEFAULT_ENTRY;
-  while(filesize > 0) {
-    pa = new_page();//create a new physical page
-    _map(as, va, pa);//map virtual to physical
-    va += PGSIZE;//add va
-    fs_read(fd, pa, PGSIZE);//read file content, and fill in this physical page
-    filesize -= PGSIZE;
-  }
+	printf("fd = %d\n", fd);
+	fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd)); 
+	fs_close(fd); 
 
   return (uintptr_t)DEFAULT_ENTRY;
 }
