ff0c00ab6c19c948ddb2554b5d949d24512b1821
tracer-ics2017 <tracer@njuics.org>
2018-06-05 17:04:26 +0800

    >  run 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:04:26 up  4:49,  1 user,  load average: 0.14, 0.24, 0.30 e8665221a12a3971294fc82ede53b9cab1cac591

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,9 +8,10 @@ uintptr_t sys_write(int fd, const void *buf, size_t len) {
 	if (fd==1||fd==2){
 		for(;len>0;len--,i++){
 			_putc(((char*)buf)[i]);
-      Log("it's log\n");
+      
 		}
 	}
+  Log("it's log\n");
 	return i;
 }
 
