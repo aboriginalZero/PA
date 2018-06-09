dfc1a248a260b71b95a8bee88fbb6c14be3615ff
tracer-ics2017 <tracer@njuics.org>
2018-06-09 17:50:54 +0800

    >  run 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:50:54 up  5:04,  1 user,  load average: 1.12, 0.59, 0.44 489bf633cdd7290da12eb1528c2f76a0f4f80054

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -34,35 +34,40 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-  int x, y;
-	int len1, len2, len3;
-	//offset /= 4;
-	offset = offset >> 2;
-	y = offset / _screen.width;
-	x = offset % _screen.width;
+  // int x, y;
+	// int len1, len2, len3;
+	// //offset /= 4;
+	// offset = offset >> 2;
+	// y = offset / _screen.width;
+	// x = offset % _screen.width;
 	
-  //Log("fb_write x:%d y:%d len:%d\n", x, y, len);
-	//len /= 4;
-	len = len >> 2;
-	len1 = len2 = len3 = 0;
+  // //Log("fb_write x:%d y:%d len:%d\n", x, y, len);
+	// //len /= 4;
+	// len = len >> 2;
+	// len1 = len2 = len3 = 0;
 	
-	// the first line
-	len1 = len <= _screen.width - x ? len : _screen.width - x;
-	_draw_rect((uint32_t *)buf, x, y, len1, 1);
+	// // the first line
+	// len1 = len <= _screen.width - x ? len : _screen.width - x;
+	// _draw_rect((uint32_t *)buf, x, y, len1, 1);
 
-	// the middle line
-	if (len > len1 && ((len - len1) > _screen.width))	{
-		//len2 = (len - len1) / _screen.width * _screen.width;
-		len2 = len - len1;
-		_draw_rect((uint32_t *)buf + len1, 0, y + 1, _screen.width, len2 / _screen.width);
-	}	
+	// // the middle line
+	// if (len > len1 && ((len - len1) > _screen.width))	{
+	// 	//len2 = (len - len1) / _screen.width * _screen.width;
+	// 	len2 = len - len1;
+	// 	_draw_rect((uint32_t *)buf + len1, 0, y + 1, _screen.width, len2 / _screen.width);
+	// }	
 
-	// the lase line
-	if (len - len1 - len2 > 0) {
-		len3 = len - len1 - len2;
-		_draw_rect((uint32_t *)buf + len1 + len2, 0, y + len2 / _screen.width + 1, len3, 1);
-	}
-
+	// // the lase line
+	// if (len - len1 - len2 > 0) {
+	// 	len3 = len - len1 - len2;
+	// 	_draw_rect((uint32_t *)buf + len1 + len2, 0, y + len2 / _screen.width + 1, len3, 1);
+	// }
+int width = _screen.width;
+	len /= 4;
+	offset /= 4;
+	int y = (offset) / width;
+	int x = (offset) % width;
+	_draw_rect((uint32_t *)(buf), x, y, len, 1);
 }
 
 void init_device() {
