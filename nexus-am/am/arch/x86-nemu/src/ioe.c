04df852cda492a1209f055bfae7754baa001cd51
tracer-ics2017 <tracer@njuics.org>
2018-06-14 14:33:10 +0800

    >  run 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:33:10 up  2:23,  1 user,  load average: 0.25, 0.57, 0.91 b4d6d9322d8577c91d5778c4cb9042cd53ad68f

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -33,9 +33,6 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
       fb[i*_screen.width+j]=pixels[k++];
     }
   }
-  // for (int i=0;i<h;i++) {
-	// 	memcpy(fb+(y + i)*_screen.width + x,pixels+i*w,w*4);
-	// }
 }
 
 void _draw_sync() {
