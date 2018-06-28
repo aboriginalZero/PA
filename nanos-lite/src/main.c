24818c3f2db63f77ba5814d9c2bdd9db1fc7c29f
tracer-ics2017 <tracer@njuics.org>
2018-06-28 17:00:32 +0800

    >  run 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:18:27 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:00:32 up 32 min,  1 user,  load average: 0.93, 0.59, 0.70 102767351af11fe65f4961e3e3213600be950c5e

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,7 @@ int main() {
   // uint32_t entry = loader(NULL, "/bin/dummy");
   // ((void (*)(void))entry)();
   load_prog("/bin/pal");
-
+  Log(" 333333...");
   _trap();
 
   panic("Should not reach here");
