e5e9b17fa1ef235963c3462faa4243ee1cc6c835
tracer-ics2017 <tracer@njuics.org>
2018-06-30 12:49:27 +0800

    >  run 161630324 caiyiwu Linux caiyiwu-CW65S 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:18:27 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:49:27 up 1 day, 16:55,  1 user,  load average: 0.47, 0.18, 0.18 2c44157a1f14a07a209a90ea2297cac00d53c6

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,6 +8,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:{
       do_syscall(r);
+      ret =schedule(r);
       break;
     }
     case _EVENT_TRAP:{
